e001ba685eeb25868cdcde577ccc6112e5ff6701
N1Alice <kc700370@gmail.com>
2023-11-20 14:44:21 +0900

    setting

---
+++ src/main/java/com/ll/sb_re231120/SbRe231120Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb_re231120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRe231120Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRe231120Application.class, args);
+    }
+
+}
