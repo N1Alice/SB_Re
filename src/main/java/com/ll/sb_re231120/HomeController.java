4d02936b1f186caa8ad0d3e88ab17c347b53f25a
N1Alice <kc700370@gmail.com>
2023-11-21 13:48:15 +0900

    Calc3 ~ 5

--- src/main/java/com/ll/sb_re231120/HomeController.java
+++ src/main/java/com/ll/sb_re231120/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb_re231120;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -30,5 +31,32 @@ public class HomeController {
     @ResponseBody
     String showCalc2(Integer a, Integer b){
         return "a :" + a + ", b : " + b;
+    }
+
+    @GetMapping("/Calc3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산결과 : %d" .formatted(a + b);
+    }
+
+    @GetMapping("/Calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산결과 : %f" .formatted(a + b);
+    }
+
+    @GetMapping("/Calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "0") String a,
+            @RequestParam(defaultValue = "0") String b
+    ) {
+        return "계산결과 : %s" .formatted(a + b);
     }
 }
