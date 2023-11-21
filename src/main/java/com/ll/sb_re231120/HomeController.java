b24ef2c23925809c7c1015dfcafe802e648e9d89
N1Alice <kc700370@gmail.com>
2023-11-21 15:38:48 +0900

    긴응답

--- src/main/java/com/ll/sb_re231120/HomeController.java
+++ src/main/java/com/ll/sb_re231120/HomeController.java
@@ -142,6 +142,79 @@ public class HomeController {
         }};
         return persons;
     }
+
+    @GetMapping("/Calc14")
+    @ResponseBody
+    String showCalc14() {
+        String html = "";
+        html += "<div>";
+        html += "<input type=\"text\" placeholder=\"내용\">";
+        html += "</div>";
+
+        return html;
+    }
+
+    @GetMapping("/Calc15")
+    @ResponseBody
+    String showCalc15() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<div>");
+        sb.append("<input type=\"text\" placeholder=\"내용\">");
+        sb.append("</div>");
+//결과는 같다
+        return sb.toString();
+    }
+
+    @GetMapping("/Calc16")
+    @ResponseBody
+    String showCalc16() {
+        String html = "<div><input type=\"text\" placeholder=\"내용\"></div>";
+// 14는 않좋고 15는 ㄱㅊ하고 16은 한큐에 할수있음 한다 더길게 되면 """ """사용
+        return html;
+    }
+
+    @GetMapping("/Calc17")
+    @ResponseBody
+    String showCalc17() {
+        String html = """
+                <div>
+                    <input type="text" placeholder="내용">
+                </div>
+                """;
+//개행문자 없어도 된다.
+        return html;
+    }
+
+    @GetMapping("/Calc18")
+    @ResponseBody
+    String showCalc18() {
+        String html = """
+                <div>
+                    <input type="text" placeholder="내용" value="반가워요.">
+                </div>
+                """;
+
+        return html;
+    }
+
+    @GetMapping("/Calc19")
+    @ResponseBody
+    String showCalc19(
+            @RequestParam(defaultValue = "") String subject,
+            @RequestParam(defaultValue = "") String content
+    ) {
+        String html = """
+                <div>
+                    <input type="text" placeholder="제목" value="%s">
+                </div>
+                <div>
+                    <input type="text" placeholder="내용" value="%s">
+                </div>
+                """.formatted(subject, content);
+
+        return html;
+    }
 }
 
 @AllArgsConstructor
