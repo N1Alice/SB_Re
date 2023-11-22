0dd698e8990698c43ada14d76d452b897ed613fa
N1Alice <kc700370@gmail.com>
2023-11-22 11:18:26 +0900

    article/write, step9, RsData, Object

--- src/main/java/com/ll/sb_re231120/ArticleController.java
+++ src/main/java/com/ll/sb_re231120/ArticleController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ArticleController {
@@ -19,22 +17,25 @@ public class ArticleController {
     String showWrite() {
         return "article/write";
     }
+
     @GetMapping("/article/doWrite")
     @ResponseBody
-    Map<String, Object> doWrite(
+    RsData doWrite(
             String title,
             String body
     ) {
-        Article article = new Article(articles.size()+1, title, body);
+        Article article = new Article(articles.size() + 1, title, body);
 // 자동으로 크기 확장
-        Map<String, Object> rs = new LinkedHashMap<>();
-        //해쉬맵은 순서를 기억못한다, 링크드 해쉬맵은 순서를 기억한다.
-        rs.put("resultCode", "S-1");
-        rs.put("msg", "%d번 게시물이 작성되었습니다.".formatted(article.getId()));
-        rs.put("data", article);
+        RsData rs = new RsData(
+                "S-1",
+                "%d번 게시물이 작성되었습니다.",
+                article
+        );
 
-        articles.add(article);
-
+        String resultCode = rs.getResultCode();
+        String msg = rs.getMsg();
+        Article _article = (Article) rs.getData();
+//추상적에서 구체적으로 갈때는(형변환) (Article) 해줘야한다 그냥 외워라 약속이다
         return rs;
     }
 
@@ -51,10 +52,19 @@ public class ArticleController {
     }
 }
 
-    @AllArgsConstructor
-    @Getter
-    class Article {
-        private long id;
-        private String title;
-        private String body;
-    }
+@AllArgsConstructor
+@Getter
+class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+    //옛날에는 오브젝트 사용함
+}
+
+@AllArgsConstructor
+@Getter
+class Article {
+    private long id;
+    private String title;
+    private String body;
+}
