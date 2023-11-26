afb0423a21ca7236e1245413d4ac82f4c73d291c
N1Alice <kc700370@gmail.com>
2023-11-27 01:54:20 +0900

    article/write step34, ArticleRepository

--- src/main/java/com/ll/sb_re231120/domain/article/article/Service/ArticleService.java
+++ src/main/java/com/ll/sb_re231120/domain/article/article/Service/ArticleService.java
@@ -1,27 +1,30 @@
 package com.ll.sb_re231120.domain.article.article.Service;
 
 import com.ll.sb_re231120.domain.article.article.entity.Article;
+import com.ll.sb_re231120.domain.article.article.repository.ArticleRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleService {
-    private final List<Article> articles = new ArrayList<>();
+    private final ArticleRepository articleRepository = new ArticleRepository();
+
 
     public Article write(String title, String body) {
 
-        Article article = new Article(articles.size() + 1, title, body);
-        articles.add(article);
+        Article article =
+                new Article(title, body);
+
+        articleRepository.save(article);
 
         return article;
 
     }
 
     public Article findLastArticle() {
-        return articles.getLast();
+        return articleRepository.findLastArticle();
     }
 
     public List<Article> findAll() {
-        return articles;
+        return articleRepository.findAll();
     }
 }
