999267a19983b9f1593d26000ccf2ff1bc5e44b4
N1Alice <kc700370@gmail.com>
2023-11-28 15:39:13 +0900

    step 43, Bean

---
+++ src/main/java/com/ll/sb_re231120/global/app/Appconfig.java
@@ -0,0 +1,15 @@
+package com.ll.sb_re231120.global.app;
+
+import com.ll.sb_re231120.domain.article.article.entity.Article;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class Appconfig {
+    @Bean
+    List<Article> articles() {
+        return new java.util.LinkedList<>();
+    }
+}
