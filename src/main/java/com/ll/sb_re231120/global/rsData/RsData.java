285f1ad6ebb5090970ddb3fe9cdd3b674caffc52
N1Alice <kc700370@gmail.com>
2023-11-30 09:43:07 +0900

    step 51, case

--- src/main/java/com/ll/sb_re231120/global/rsData/RsData.java
+++ src/main/java/com/ll/sb_re231120/global/rsData/RsData.java
@@ -2,12 +2,14 @@ package com.ll.sb_re231120.global.rsData;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class RsData<T> {
-    private String resultCode;
-    private String msg;
+    private final String resultCode;
+    private final String msg;
     private T data;
     //미완성 시켜라, T는 정해진것이 아닌 임의
 }
