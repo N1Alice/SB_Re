49e02ce29b1c825798f8b1e03c21dee8a28c0f37
N1Alice <kc700370@gmail.com>
2023-12-01 00:48:51 +0900

    step 66, testdata

--- src/main/java/com/ll/sb_re231120/global/app/Appconfig.java
+++ src/main/java/com/ll/sb_re231120/global/app/Appconfig.java
@@ -1,20 +1,7 @@
 package com.ll.sb_re231120.global.app;
 
-import com.ll.sb_re231120.domain.article.article.entity.Article;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class Appconfig {
-    @Bean
-    List<Article> articles2() {
-        return new java.util.LinkedList<>();
-    }
-
-    @Bean
-    List<Article> articles(){
-        return new java.util.ArrayList<>();
-    }
 }
