fe09a547bbf60d7895d1510bd4e9960a92d93a43
N1Alice <kc700370@gmail.com>
2023-12-02 21:53:35 +0900

    step 78, delete

--- src/main/java/com/ll/sb_re231120/domain/article/article/Service/ArticleService.java
+++ src/main/java/com/ll/sb_re231120/domain/article/article/Service/ArticleService.java
@@ -34,4 +34,8 @@ public class ArticleService {
     public Optional<Article> findById(long id) {
         return articleRepository.findById(id);
     }
+
+    public void delete(long id) {
+        articleRepository.delete(id);
+    }
 }
