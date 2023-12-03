863110444d504c2aa770c35058dacb061e62deab
N1Alice <kc700370@gmail.com>
2023-12-03 18:00:21 +0900

    step 38

---
+++ src/main/java/com/ll/sb_re231120/global/webMvc/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.ll.sb_re231120.global.webMvc;
+
+import com.ll.sb_re231120.global.interceptor.NeedToAdminInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final NeedToAdminInterceptor needToAdminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(needToAdminInterceptor)
+                .addPathPatterns("/adm/**");
+    }
+}
