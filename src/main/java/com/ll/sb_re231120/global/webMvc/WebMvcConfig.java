eaeba72cf6872038e718f40977c651d26de9131a
N1Alice <kc700370@gmail.com>
2023-12-03 19:08:36 +0900

    step 44

--- src/main/java/com/ll/sb_re231120/global/webMvc/WebMvcConfig.java
+++ src/main/java/com/ll/sb_re231120/global/webMvc/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.ll.sb_re231120.global.webMvc;
 
 import com.ll.sb_re231120.global.interceptor.NeedToAdminInterceptor;
 import com.ll.sb_re231120.global.interceptor.NeedToLoginInterceptor;
+import com.ll.sb_re231120.global.interceptor.NeedToLogoutInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,11 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final NeedToLogoutInterceptor needToLogoutInterceptor;
     private final NeedToLoginInterceptor needToLoginInterceptor;
     private final NeedToAdminInterceptor needToAdminInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(needToLogoutInterceptor)
+                .addPathPatterns("/member/login")
+                .addPathPatterns("/member/join")
+                .addPathPatterns("/member/findUsername")
+                .addPathPatterns("/member/findPassword");
         registry.addInterceptor(needToLoginInterceptor)
                 .addPathPatterns("/adm/**");
         registry.addInterceptor(needToAdminInterceptor)
