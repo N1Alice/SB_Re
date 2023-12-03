ff8d277585f8bbda4423336eb0e04909af6e72de
N1Alice <kc700370@gmail.com>
2023-12-03 22:20:51 +0900

    step 13

--- src/main/java/com/ll/sb_re231120/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/sb_re231120/domain/member/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ll.sb_re231120.domain.member.member.service;
 import com.ll.sb_re231120.domain.member.member.entity.Member;
 import com.ll.sb_re231120.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,31 +14,27 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member join(String username, String password) {
+        password = passwordEncoder.encode(password);
         Member member = new Member(username, password);
 
         memberRepository.save(member);
-
         return member;
     }
-
     public List<Member> findAll() {
         return memberRepository.findAll();
     }
-
     public Optional<Member> findById(long id) {
         return memberRepository.findById(id);
     }
-
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
-
     public void delete(long id) {
         memberRepository.delete(id);
     }
-
     public void modify(long id, String username, String password) {
         Member member = findById(id).get();
         member.setUsername(username);
