4a22625ec0a732e258f2df828fa47ef9c6287772
N1Alice <kc700370@gmail.com>
2023-12-03 20:52:44 +0900

    step 3

--- src/main/java/com/ll/sb_re231120/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb_re231120/domain/home/home/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.ll.sb_re231120.domain.home.home.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 // 컨트롤러 입니다. 스프링부트와 의 약속
@@ -9,6 +15,18 @@ public class HomeController {
     @GetMapping("/")
     public String goToArticleList() {
         return "redirect:/article/list";
+    }
+
+    @GetMapping("/home/session")
+    @ResponseBody
+    public Map<String, Object> showSession(HttpSession session) {
+        return Collections.list(session.getAttributeNames()).stream()
+                .collect(
+                        Collectors.toMap(
+                                key -> key,
+                                key -> session.getAttribute(key)
+                        )
+                );
     }
 }
 
