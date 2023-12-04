daa3dd7c607883e3e3cc0d97434c2b935b0f4593
N1Alice <kc700370@gmail.com>
2023-12-05 03:01:07 +0900

    step 11

---
+++ src/main/java/com/ll/sb_re231120/domain/base/att/repository/AttrRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sb_re231120.domain.base.att.repository;
+
+import com.ll.sb_re231120.domain.base.attr.entity.Attr;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttrRepository extends JpaRepository<Attr, Long> {
+}
