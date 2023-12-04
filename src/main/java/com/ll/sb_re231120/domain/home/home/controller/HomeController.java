3f2b18e37381a24e2b52a6f082977128a49a9471
N1Alice <kc700370@gmail.com>
2023-12-04 13:27:00 +0900

    step 39

--- src/main/java/com/ll/sb_re231120/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb_re231120/domain/home/home/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.ll.sb_re231120.domain.home.home.controller;
 
+import com.ll.sb_re231120.global.rq.Rq;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +13,9 @@ import java.util.stream.Collectors;
 
 @Controller
 // 컨트롤러 입니다. 스프링부트와 의 약속
+@RequiredArgsConstructor
 public class HomeController {
+    private final Rq rq;
     @GetMapping("/")
     public String goToArticleList() {
         return "redirect:/article/list";
